ef0c34ac999e25836f4ad940c8525389fc396964
yanze.mei <yanze.mei@mihoyo.com>
2017-08-24 15:00:37 +0800

    实现提取公共权限的功能 输出到文件时统一前加一个\t

--- Deduplication_manifest.sh
+++ Deduplication_manifest.sh
@@ -30,9 +30,9 @@ function deduplication()
 				contentArray=(${contentArray[@]} $lineNoSpace)
 				if [ "$DONE" = "true" ]
 				then
-					echo -n "$line">>"$3"
+					echo -ne "\t$line">>"$3"
 				else
-					echo "$line">>"$3"
+					echo -e "\t$line">>"$3"
 				fi
 			fi
 		fi
@@ -51,8 +51,12 @@ function deduplication()
 CHANNEL_PATH=Ext/
 
 CONDITION="<uses-permission.*/>"
-OUTPUT_FILE=list_manifest.xml
-INPUT_FILE=AndroidManifest.xml
+LIST_MANIFEST=list_manifest.xml
+ANDROID_MANIFEST=AndroidManifest.xml
+NORMAL_MANIFEST=normal.xml
+
+android_dir_num=0
+temp="temp.xml"
 
 cd $CHANNEL_PATH
 if [ ! $? -eq 0 ];then
@@ -61,25 +65,53 @@ if [ ! $? -eq 0 ];then
 	exit 1
 fi
 
-echo -n "">$OUTPUT_FILE
+echo "---start listing info of manifest to list_manifest.xml---"
+echo -n "">$LIST_MANIFEST
 find . -type d -maxdepth 1 | while read channel_name
 do
 	channel_name=`echo $channel_name | sed 's/.\///'`
-	echo "[${channel_name}]:">>$OUTPUT_FILE
+	echo "[${channel_name}]:">>$LIST_MANIFEST
 	if [ ! -d ${channel_name}/Android ];then
-		echo -e "\tNone">>$OUTPUT_FILE
+		echo -e "\tNone">>$LIST_MANIFEST
 	else
-		deduplication "$CONDITION" "${channel_name}/Android/${INPUT_FILE}" "${OUTPUT_FILE}"
+		deduplication "$CONDITION" "${channel_name}/Android/${ANDROID_MANIFEST}" "${LIST_MANIFEST}"
+		android_dir_num=$[$android_dir_num+1]
+		echo -n $android_dir_num>$temp
 	fi
+	echo "$channel_name done"
 done
+android_dir_num=$(cat $temp)
+rm -f $temp
+echo "---done---"
 
-
-# 统计渠道个数
-# 统计输出为none的个数
-# 二者之差为有多少个android文件夹
+# 统计有多少个android文件夹
 # 使用桶排序的思想统计每种权限出现次数，等于上述个数的即为公共权限
-
-
-
+echo "---start listing common content to noarmal.xml---"
+contentNum=()
+echo -n "">$NORMAL_MANIFEST
+DONE="false"
+until [ "$DONE" = "true" ]
+do read line || DONE="true"
+	if [[ $line =~ $CONDITION ]]
+	then
+		lineNoSpace=`echo "$line"|sed 's/ //g'`
+		if [[ ${contentArray[@]} =~ $lineNoSpace ]]
+		then
+			for i in `seq 0 $[${#contentArray[@]}-1]`
+			do
+				if [ $lineNoSpace = ${contentArray[$i]} ];then
+					contentNum[$i]=$[${contentNum[$i]}+1]
+					if [[ ${contentNum[$i]} -eq $android_dir_num ]]; then
+						echo -e "\t$line">>$NORMAL_MANIFEST
+					fi
+				fi
+			done
+		else
+			contentArray=(${contentArray[@]} $lineNoSpace)
+			contentNum[$[${#contentArray[@]}-1]]=1
+		fi
+	fi
+done < $LIST_MANIFEST
+echo "---done---"
 
 read a
